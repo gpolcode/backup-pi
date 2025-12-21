db771a7da68184468cc2e1204d232d718514504c
gpolcode <gl.polenta@gmail.com>
2025-12-21 13:18:07 +0100

    increase cache

--- opt/unlock.sh
+++ opt/unlock.sh
@@ -6,7 +6,7 @@ rclone mount gdrive: /mnt/data/rclone/gdrive \
   --read-only \
   --cache-dir /mnt/tmpfs \
   --vfs-cache-mode full \
-  --vfs-cache-max-size 1G \
+  --vfs-cache-max-size 1.9G \
   --vfs-read-ahead 128M \
   --vfs-read-chunk-streams 64 \
   --vfs-read-chunk-size 16M
