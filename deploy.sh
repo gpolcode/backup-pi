0caed60f8e953e72df56342c0630e19bd1cf69b5
gpolcode <gl.polenta@gmail.com>
2026-01-10 13:36:07 +0100

    root only

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+umask 077
 wget https://github.com/gpolcode/backup-pi/archive/refs/heads/main.tar.gz -O /tmp/main.tar.gz
 tar -xzf /tmp/main.tar.gz -C /tmp
 tar -C /tmp/backup-pi-main -cf - . | tar -C / -xpf -
