d20dd8b8f2138461b01c07140bf5dd56426f78c7
gpolcode <gl.polenta@gmail.com>
2026-02-05 19:13:14 +0100

    spaces

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ tar -xzf /tmp/main.tar.gz -C /tmp
 
 SRC=/tmp/backup-pi-main
 find "$SRC" -type f | while read -r f; do
-    cp -f "$f" "/${f#$SRC/}"
-    chmod 700 "/${f#$SRC/}"
+  cp -f "$f" "/${f#$SRC/}"
+  chmod 700 "/${f#$SRC/}"
 done
 
 if [ -z "$PING_URL" ]; then
-    read -s -p "Enter healthcheck.io ping_key: " PING_KEY
-    printf "export PING_URL='https://hc-ping.com/$PING_KEY'"> /root/backup-pi.env
+  read -s -p "Enter healthcheck.io ping_key: " PING_KEY
+  printf "export PING_URL='https://hc-ping.com/$PING_KEY'"> /root/backup-pi.env
 fi
