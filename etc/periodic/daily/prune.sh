c173ba2a7a3489eb24a18c74f407ae9a31d1aacb
gpolcode <gl.polenta@gmail.com>
2026-02-05 19:20:16 +0100

    add monthly snapshots

--- etc/periodic/daily/prune.sh
+++ etc/periodic/daily/prune.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 . "$HOME/.profile"
 
-output="$(restic forget --keep-last 50 --keep-hourly 24 --keep-daily 7 --keep-weekly 4 --keep-yearly 3 --prune --retry-lock 10m 2>&1)"
+output="$(restic forget --keep-last 62 --keep-hourly 24 --keep-daily 7 --keep-weekly 4 --keep-monthly 12 --keep-yearly 3 --prune --retry-lock 10m 2>&1)"
 rc=$?
 
 if [ "$rc" -ne 0 ]; then
