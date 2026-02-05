89d89acfd75ca4e3ddecf8c7b6caa394beb222dc
gpolcode <gl.polenta@gmail.com>
2026-02-05 18:44:39 +0100

    check for empty backup

--- etc/periodic/hourly/backup.sh
+++ etc/periodic/hourly/backup.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 umask 077
 . "$HOME/.profile"
-
-wget "$PING_URL/start" -O /dev/null
-
-if ! mountpoint -q "$RCLONE_PATH"; then
-  wget --post-data "Mount missing: $RCLONE_PATH" "$PING_URL/fail" -O /dev/null || true
-  exit 1
-fi
-
 TMP="$(mktemp)"
 trap 'rm -f "$TMP"' EXIT
+
+wget "$PING_URL/start" -O /dev/null
 
 restic backup "$RCLONE_PATH" -v --retry-lock 10m >"$TMP" 2>&1
 rc=$?
 
-if [ "$rc" -eq 0 ]; then
-  wget --post-file "$TMP" "$PING_URL" -O /dev/null
-else
+if [ "$rc" -ne 0 ]; then
   wget --post-file "$TMP" "$PING_URL/fail" -O /dev/null || true
+  exit 1
 fi
+
+snapshot="$(restic snapshots latest --json | grep '"total_files_processed":0')"
+if [ ! -z "$snapshot" ]; then
+  wget --post-data "Backup failed with no files processed" "$PING_URL/fail" -O /dev/null || true
+  exit 1
+fi
+
+wget --post-file "$TMP" "$PING_URL" -O /dev/null
