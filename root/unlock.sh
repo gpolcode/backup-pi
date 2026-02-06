6502508db33350abb76ea09118452ad464f732eb
gpolcode <gl.polenta@gmail.com>
2026-02-06 20:20:04 +0100

    remove rc api and lower cache time

--- root/unlock.sh
+++ root/unlock.sh
@@ -10,12 +10,10 @@ rclone mount gdrive: "$RCLONE_PATH" \
   --daemon \
   --read-only \
   --cache-dir /tmp/gdrive \
-  --rc \
-  --rc-addr 127.0.0.1:5572 \
   --vfs-cache-mode full \
   --vfs-cache-max-size 1G \
-  --vfs-cache-max-age 24h \
-  --dir-cache-time 24h \
+  --vfs-cache-max-age 10m \
+  --dir-cache-time 10m \
   --attr-timeout 5s \
   --poll-interval 1m \
   --vfs-refresh \
