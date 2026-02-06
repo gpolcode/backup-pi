04cab37e33a380fe21c22bac4cc92e81b37415e0
gpolcode <gl.polenta@gmail.com>
2026-02-06 19:51:52 +0100

    possible rclone performance improvement

--- root/unlock.sh
+++ root/unlock.sh
@@ -6,12 +6,19 @@ printf '%s' "$password" > /tmp/backup-pass
 chmod 400 /tmp/backup-pass
 unset password
 
-rclone mount gdrive: $RCLONE_PATH \
+rclone mount gdrive: "$RCLONE_PATH" \
   --daemon \
   --read-only \
   --cache-dir /tmp/gdrive \
+  --rc \
+  --rc-addr 127.0.0.1:5572 \
   --vfs-cache-mode full \
   --vfs-cache-max-size 1G \
+  --vfs-cache-max-age 24h \
+  --dir-cache-time 24h \
+  --attr-timeout 5s \
+  --poll-interval 1m \
+  --vfs-refresh \
   --vfs-read-ahead 128M \
   --vfs-read-chunk-streams 64 \
   --vfs-read-chunk-size 16M
