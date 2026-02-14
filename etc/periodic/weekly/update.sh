e182ccf59937a6ceceba20d7cee859bd005a6314
gpolcode <gl.polenta@gmail.com>
2026-02-14 13:42:22 +0100

    check update result

--- etc/periodic/weekly/update.sh
+++ etc/periodic/weekly/update.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-apk update && apk upgrade && reboot
+. "$HOME/.profile"
+
+output="$(
+  apk update 2>&1 &&
+  apk upgrade 2>&1
+)"
+rc=$?
+
+if [ "$rc" -ne 0 ]; then
+  wget --post-data "$output" "$PING_URL/fail" -O /dev/null || true
+  exit 1
+fi
+
+reboot
