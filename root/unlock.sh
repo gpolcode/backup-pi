4e56b9b92092fecee9baed76e7f52c548bcf05f9
gpolcode <gl.polenta@gmail.com>
2026-03-30 17:12:35 +0200

    disable caching

--- root/unlock.sh
+++ root/unlock.sh
@@ -9,13 +9,6 @@ unset password
 rclone mount gdrive: "$RCLONE_PATH" \
   --daemon \
   --read-only \
-  --cache-dir /tmp/gdrive \
-  --vfs-cache-mode full \
-  --vfs-cache-max-size 1G \
-  --vfs-cache-max-age 2h \
-  --dir-cache-time 1000h \
-  --attr-timeout 1s \
-  --poll-interval 15s \
   --vfs-read-ahead 128M \
   --vfs-read-chunk-streams 64 \
   --vfs-read-chunk-size 16M
